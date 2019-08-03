5e4b28110ec17333d0145289d4f47d39b43725b1
chengyin <chengyinok@qq.com>
2019-08-03 17:23:43 +0800

    Add

--- src/views/common/GlobalLayout.vue
+++ src/views/common/GlobalLayout.vue
@@ -1,6 +1,10 @@
 <template>
   <a-layout>
+    <a-layout-sider :trigger="null"
+                    collapside
+                    v-model="collapsed">
 
+    </a-layout-sider>
   </a-layout>
 </template>
 <script>
